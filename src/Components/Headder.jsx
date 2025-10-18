945673e0dae69cd88e23221e1685705b67d40791
Asikur Rahman <mdasikur5893@gmail.com>
2025-10-18 15:52:36 +0600

    react fns use

--- src/Components/Headder.jsx
+++ src/Components/Headder.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import logo from '../assets/logo.png'
+import { format } from 'date-fns';
+
+
 
 const Headder = () => {
     return (
         <div className='flex flex-col justify-center items-center'>
           <img className='w-[350px]' src={logo} alt="" />
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates, laudantium!</p>
+          <p> sit amet consectetur adipisicing elit. Voluptates, laudantium!</p>
+       
+          {/* react fns use first time.. date time year ar janno.. */}
+
+          <p> {format(new Date(),'EEEE,MMMM MM,yyyy')}</p>
         </div>
     );
 };
